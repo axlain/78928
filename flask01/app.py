3f87750febc22291941781815028b7b67c313f29
Alain Vasquez Ramirez <zs22016095@estudiantes.uv.mx>
2025-03-07 10:38:46 -0600

    rutas en flask01

--- flask01/app.py
+++ flask01/app.py
@@ -4,8 +4,24 @@ app = Flask(__name__)
 
 @app.route('/')
 
-def hola_mundo():
-    return 'Hola Mundo!'
+def hola_aaron():
+    return 'Hola Aaron!'
+
+@app.route('/alex')
+def hola_alex():
+    return 'Hola Alex!'
+
+@app.route('/jorge')
+def hola_jorge():
+    return '<h1 style="color:red;">Hola jorge!</h1>'
+
+@app.route('/json')
+def algo():
+    return '{"nombre":"John"}'
+
+@app.route('/xml')
+def algo2():
+    return '<nombre>Jonh</nombre>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True) 
